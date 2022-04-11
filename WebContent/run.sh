b372c4f4cce39cb1900c457c01c64717aa8b3fa5
Armando Solar-Lezama <asolar@mit.edu>
2022-04-11 18:48:02 -0400

    Updated style and fixed typos

---
+++ WebContent/run.sh
@@ -0,0 +1,7 @@
+#If you have python installed, you can run this file to display your downloaded project.
+#For most projects, you can just open the view.html file directly, but most modern browsers
+#are very picky when it comes to opening files directly from your local file system, so 
+#in particular, projects that use the playskript 'canvas' construct might not display 
+#correctly unless you run them through a web server using this command. 
+
+python -m http.server 8080  | python -mwebbrowser http://localhost:8080/index.html
