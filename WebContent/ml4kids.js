e8c77cd6e9ac625a0362fc692a9fd7f7185c6acf
Armando Solar-Lezama <asolar@mit.edu>
2022-05-31 23:42:28 -0400

    Starting ml4kids project

---
+++ WebContent/ml4kids.js
@@ -0,0 +1,142 @@
+
+
+var module = {};
+
+const server = {
+    get: function (command, onok, onerror) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState == 4) {
+                if (this.status == 200) {
+                    const response = xhttp.responseText;
+                    if (response.substring(0, 2) === 'OK') {
+                        const jsonData = response.substring(3).trim();
+                        var rv = undefined;
+                        if (jsonData.length > 0) {
+                            rv = JSON.parse(jsonData);
+                        }
+                        if (onok) {
+                            onok(rv);
+                        }
+                    } else {
+                        if (onerror) {
+                            onerror(response);
+                        } else {
+                            errorMsg(response);
+                        }
+                    }
+                } else {
+                    var msg = errorCode.badConnection + " status:" + this.status + " state: " + this.readyState + "  " + xhttp.responseText;
+                    if (onerror) {
+                        onerror(msg);
+                    } else {
+                        errorMsg(msg);
+                    }
+                }
+            }
+        };
+        xhttp.open("GET", "https://code.playskript.com" + command, true);
+        //xhttp.open("GET", "http://localhost:4000" + command, true);
+        xhttp.send();
+    },
+
+}
+
+
+
+function sanitize(msg) {
+    return msg;
+}
+
+
+function errorMsg(msg) {
+    //msg = sanitize(msg);
+    if (document.getElementById('errMsg')) {
+        var msgText = document.getElementById('errMsgText');
+        let msgdiv = document.createElement("div");
+        msgdiv.innerHTML = msg;
+        msgdiv.setAttribute("class", "errmsg-base");
+        msgText.appendChild(msgdiv);
+        return msgdiv;
+    }
+}
+
+
+
+function readEditor() {
+    var program;
+    var editor = ace.edit("editor");
+    program = editor.getValue();
+
+    js = rapydscript.compile(program, { omit_baselib: true, beautify: true })
+    js = rapydToPsk(js)
+
+    console.log(js);
+    
+    return { code: js, version: 1, minor: 1 };
+}
+
+
+
+
+
+
+function rapydToPsk(rapyd) {
+    class CodeFixer extends PrettyPrinter {
+        visitNum(expr) {
+            if (expr.val >= 0) {
+                return '#' + expr.val;
+            } else {
+                return '-#' + Math.abs(expr.val);
+            }
+        }
+        visitVar(expr) {
+            let renames = {
+                'track0': 'cars.track0', 
+                'track1': 'cars.track1', 
+                'track2': 'cars.track2', 
+                'track3': 'cars.track3', 
+                'track4': 'cars.track4', 
+                'track5': 'cars.track5',
+                'newCar': 'cars.newCar',
+                'displayTrack': 'cars.displayTrack',
+                'simulate': 'cars.simulate',
+                'displayResult': 'cars.displayResult',
+                'displayCar':'cars.displayCar'
+            };
+            let nf = nameFilter(expr.name);
+
+            if (nf in renames) {
+                return renames[nf];
+            }
+
+            return nf;
+        }
+    }
+
+
+
+    
+    rapyd =  rapyd.replace(/var[^;]*;/g, "//NO MORE");
+    rapyd = rapyd.replace('"use strict";\n', "");
+    while ((/^\s*\(function\(\)\{/).test(rapyd) && (/\}\)\(\);\s*$/).test(rapyd)) {
+        rapyd = rapyd.replace(/^\s*\(function\(\)\{/g, "");
+        rapyd = rapyd.replace(/\}\)\(\);\s*$/g, "");
+    }
+    rapyd = rapyd.replace(/.._print/g, "alert");  
+    rapyd = rapyd.replace(/true_./g, "true");  
+    rapyd = rapyd.replace(/false_./g, "false");  
+
+    let ast = Parser().program(rapyd);
+    let fixer = new CodeFixer();
+
+    rapyd = ast.accept(fixer);
+
+    return rapyd;
+    let code = new CodeText(rapyd).iter();
+    if (lookahead('(function()', code)) {
+
+
+    }
+
+}
